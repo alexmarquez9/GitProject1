5e90528dbf02d8722ef8e099ddf825da7f2a1e86
alexmarquez9 <140654902+alexmarquez9@users.noreply.github.com>
2023-07-29 23:14:39 -0400

    Update DemoDay6.java

--- src/day6/DemoDay6.java
+++ src/day6/DemoDay6.java
@@ -5,4 +5,7 @@ public class DemoDay6 {
 	public static void main(String[] args) {
 		System.out.println("Alexinho Marquizinho");
 	}
+	public static void getData() {
+		System.out.println("xyz");
+	}
 }
